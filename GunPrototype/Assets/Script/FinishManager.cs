995486fc74d277a43a607580dc8f78bde362b442
KawinM23 <kawin.m235@gmail.com>
2021-06-27 19:18:41 +0700

    Add Particle Material

--- GunPrototype/Assets/Script/FinishManager.cs
+++ GunPrototype/Assets/Script/FinishManager.cs
@@ -25,10 +25,6 @@ public class FinishManager : MonoBehaviour
     [SerializeField] private GameObject border2;
     [SerializeField] private GameObject border3;
 
-    public Color star1Color;
-    public Color star2Color;
-    public Color star3Color;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -96,79 +92,16 @@ public class FinishManager : MonoBehaviour
     IEnumerator PlayStar(int star) {
         switch (star) {
             case 1:
-                border1.SetActive(true);
-                Transform border1Transform = border1.GetComponent<Transform>();
-                Vector3 localScaleChange1 = new Vector3(0.14f,0.14f,0);
-                border1Transform.localScale = new Vector3(0.3f, 0.3f, 0);
-                Vector3 deltaScale1 = border1Transform.localScale - localScaleChange1;
-
-                Vector3 deltaRotation1 = new Vector3(0, 0, 180);
-                SpriteRenderer sr1 = border1.GetComponent<SpriteRenderer>();
-                sr1.color = new Color(sr1.color.r, sr1.color.g,sr1.color.b,0.1f);
-                while (border1Transform.transform.localScale.x >= localScaleChange1.x) {
-                    border1Transform.localScale -= deltaScale1 * Time.deltaTime;
-                    border1Transform.Rotate(-deltaRotation1 * Time.deltaTime);
-                    sr1.color += new Color(0, 0, 0, 0.9f) * Time.deltaTime;
-
-                    yield return null;
-                }
-                border1Transform.rotation = new Quaternion(1, 0, 0, 0);
-                border1Transform.localScale = localScaleChange1;
-                sr1.color = new Color(sr1.color.r, sr1.color.g, sr1.color.b, 1f);
                 break;
             case 2:
-                if (allEnemiesDefeated) {
-                    border2.SetActive(true);
-                    Transform border2Transform = border2.GetComponent<Transform>();
-                    Vector3 localScaleChange2 = border2Transform.localScale;
-                    border2Transform.localScale = new Vector3(0.3f, 0.3f, 0);
-                    Vector3 deltaScale2 = border2Transform.localScale - localScaleChange2;
+                
 
-                    Vector3 deltaRotation2 = new Vector3(0, 0, 180);
-                    SpriteRenderer sr2 = border2.GetComponent<SpriteRenderer>();
-                    sr2.color = new Color(sr2.color.r, sr2.color.g, sr2.color.b, 0.1f);
-                    while (border2Transform.transform.localScale.x >= localScaleChange2.x) {
-                        border2Transform.localScale -= deltaScale2 * Time.deltaTime;
-                        border2Transform.Rotate(-deltaRotation2 * Time.deltaTime);
-                        sr2.color += new Color(0, 0, 0, 0.9f) * Time.deltaTime;
-
-                        yield return null;
-                    }
-                    border2Transform.rotation = new Quaternion(1, 0, 0, 0);
-                    border2Transform.localScale = localScaleChange2;
-                    sr2.color = new Color(sr2.color.r, sr2.color.g, sr2.color.b, 1f);
-
-                } else {
-
-                }
                 break;
             case 3:
-                if (inTime) {
-                    border3.SetActive(true);
-                    Transform border3Transform = border3.GetComponent<Transform>();
-                    Vector3 localScaleChange3 = border3Transform.localScale;
-                    border3Transform.localScale = new Vector3(0.3f, 0.3f, 0);
-                    Vector3 deltaScale3 = border3Transform.localScale - localScaleChange3;
-
-                    Vector3 deltaRotation3 = new Vector3(0, 0, 180);
-                    SpriteRenderer sr3 = border3.GetComponent<SpriteRenderer>();
-                    sr3.color = new Color(sr3.color.r, sr3.color.g, sr3.color.b, 0.1f);
-                    while (border3Transform.transform.localScale.x >= localScaleChange3.x) {
-                        border3Transform.localScale -= deltaScale3 * Time.deltaTime;
-                        border3Transform.Rotate(-deltaRotation3 * Time.deltaTime);
-                        sr3.color += new Color(0, 0, 0, 0.9f) * Time.deltaTime;
-
-                        yield return null;
-                    }
-                    border3Transform.rotation = new Quaternion(1, 0, 0, 0);
-                    border3Transform.localScale = localScaleChange3;
-                    sr3.color = new Color(sr3.color.r, sr3.color.g, sr3.color.b, 1f);
-
-                } else {
-
-                }
+                
                 break;
         }
+        yield return null;
 
 
     }
